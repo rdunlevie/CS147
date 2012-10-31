73cecdb324d1b69ef689cf83bd987817a71f2b1f
Robert Dunlevie <robdun11@stanford.edu>
2012-10-31 14:03:16 -0700

    commit message #idk

---
+++ examples/footer.php
@@ -0,0 +1,31 @@
+    <!DOCTYPE html>
+    <html>
+    
+    <div data-role="footer" data-id="samebar" class="nav-glyphish-example" data-position="fixed" data-tap-toggle="false">
+		<div data-role="navbar" class="nav-glyphish-example" data-grid="c">
+		<ul>
+			<li><a href="index.php" id="home" data-icon="custom">Home</a></li>
+			<li><a href="login.php" id="key" data-icon="custom" class="ui-btn-active">Login</a></li>
+			<li><a href="filter.php" id="coffee" data-icon="custom">Filter</a></li>
+			<li><a href="#" id="envelope" data-icon="custom">Settings</a></li>
+		</ul>
+		</div>
+	</div>
+	<script type="text/javascript">
+	$("#logout").hide();
+	$("#info").hide();
+	var retrievedObject = localStorage.getItem('username');
+	if (retrievedObject == "test") {
+		$("#form").hide();	
+		$("#logout").show();
+		$("#info").show();
+	}
+	$("#logout").click(function() {
+		localStorage.removeItem('username');
+		$("#form").show();
+		$("#logout").hide();
+		$("#info").hide();
+	});
+	</script>
+</div><!-- /page -->
+</html>
